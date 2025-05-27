d2355b58ec2cf3fc180538fe603ba6141a61f302
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-05-27 17:26:44 +0900

    feat: navigationの設定をinfiniteに追加

--- public/scripts/infinite-scroll.js
+++ public/scripts/infinite-scroll.js
@@ -1,4 +1,13 @@
 import fullpage from "fullPage.js";
 new fullpage("#fullpage", {
   licenseKey: "5N97I-9A0MK-3QK68-LX1JK-LXZLP",
+	// Navigation
+	menu: '#menu',
+	lockAnchors: false,
+	anchors:['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
+	navigation: true,
+	navigationPosition: 'right',
+	navigationTooltips: ['first slide', 'second slide', 'third slide', 'fourth slide', 'fifth slide', 'sixth slide', 'seventh slide'],
+	showActiveTooltip: false,
+	slidesNavigation: false,
 });
