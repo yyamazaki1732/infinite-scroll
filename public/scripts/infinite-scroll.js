fea538327f91f56a7610eaba794db5256f8cff67
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-05-28 18:55:38 +0900

    wip: fullPageで各sectionのアニメーションを作成中

--- public/scripts/infinite-scroll.js
+++ public/scripts/infinite-scroll.js
@@ -1,5 +1,5 @@
 import fullpage from "fullPage.js";
-new fullpage("#fullpage", {
+new fullpage("[data-fullpage-container]", {
   licenseKey: "5N97I-9A0MK-3QK68-LX1JK-LXZLP",
 	// Navigation
 	menu: '#menu',
@@ -10,4 +10,52 @@ new fullpage("#fullpage", {
 	navigationTooltips: ['first slide', 'second slide', 'third slide', 'fourth slide', 'fifth slide', 'sixth slide', 'seventh slide'],
 	showActiveTooltip: false,
 	slidesNavigation: false,
+  // Scrolling
+	css3: true,
+	scrollingSpeed: 700,
+	autoScrolling: true,
+	fitToSection: true,
+	fitToSectionDelay: 600,
+	scrollBar: false,
+	easing: 'easeInOutCubic',
+	easingcss3: 'ease', // cubic-bezier関数を使用して独自のものを作成可能
+	loopHorizontal: true,
+	continuousVertical: false,
+	continuousHorizontal: false,
+	scrollHorizontally: false,
+	interlockedSlides: false,
+	dragAndMove: false,
+	offsetSections: false,
+	resetSliders: false,
+	fadingEffect: false,
+	normalScrollElements: '#element1, .element2',
+	scrollOverflow: false,
+	scrollOverflowMacStyle: false,
+	scrollOverflowReset: false,
+	skipIntermediateItems: false,
+	touchSensitivity: 15,
+	bigSectionsDestination: null,
+	adjustOnNavChange: true,
+
+
+  continuousVertical: true,
+
+onLeave(origin, destination, direction) {
+  const h2 = destination.item.querySelector("h2");
+  console.log("onLeave", h2);
+  if (h2) {
+    h2.animate(
+      [
+        { transform: 'translateY(100px)', opacity: 0 },
+        { transform: 'translateY(0)', opacity: 1 }
+      ],
+      {
+        duration: 600,
+        fill: 'forwards',
+        easing: 'ease-in-out'
+      }
+    );
+  }
+},
+
 });
